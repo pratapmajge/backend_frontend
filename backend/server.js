22892181652a9b82333c745947967dcc9b6d9c66
pratapmajge <majgepratap1504@gmail.com>
2025-02-05 00:33:18 +0530

    complete

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from 'express'
 
 const app=express()  // first file
 
-app.get('/' ,(req ,res) =>{
-    res.send('server is ready')
-});
+// app.get('/' ,(req ,res) =>{
+//     res.send('server is ready')
+// });
 
 // get list of 5 jokes
 
