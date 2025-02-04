d15699d42a51b9ae918823ed38b91f821612daab
pratapmajge <majgepratap1504@gmail.com>
2025-02-05 00:20:21 +0530

    jokes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,39 @@ app.get('/' ,(req ,res) =>{
     res.send('server is ready')
 });
 
+// get list of 5 jokes
+
+app.get('/jokes' , (req , res) => {
+    const jokes =[
+        {
+            id :1,
+            title: ' A joke',
+            content: 'First joke'
+        },
+        {
+            id:2,
+            title:'A joke two',
+            content:'Another joke'
+        },
+        {
+            id:3,
+            title:'A joke three',
+            content:'third joke'
+        },
+        {
+            id:4,
+            title:'A joke four',
+            content:'fourth joke'
+        },
+        {
+            id:5,
+            title:'A joke five',
+            content:'fifth joke'
+        }
+    ];
+    res.send(jokes);
+});
+
 const port=process.env.PORT || 3000;
 
 app.listen(port , () =>{
