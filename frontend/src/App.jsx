ba58bc5e1f16c490ca941d708d43509d6d334784
pratapmajge <majgepratap1504@gmail.com>
2025-02-06 00:09:02 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
   const [jokes , setJokes]= useState([])  
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/jokes')
+    .then((response) =>{
+        setJokes(response.data)
+    })
+    .catch((error) =>{
+      console.log(error);
+      
+    })
+  })
   return (
     <>
       <h1>frontend </h1>
