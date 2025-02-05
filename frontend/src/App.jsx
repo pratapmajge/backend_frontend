0b1fe4f271ebf439edf0338dd5ff45308993d22e
pratapmajge <majgepratap1504@gmail.com>
2025-02-06 00:48:58 +0530

    Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,12 +24,12 @@ function App() {
       <p>Jokes: {jokes.length}</p>
 
       {
-        jokes.map((joke , index) => {
+        jokes.map((joke , index) => (
           <div key={joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
-        })
+        ))
       }
     </>
   )
